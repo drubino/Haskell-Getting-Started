b3a10adbd6eef2db6ab3034b7a4920db34a99ba7
drubino <doug.rubino@gmail.com>
2016-03-20 17:20:35 -0400

    Typeclasses in polymorphic functions

--- types.hs
+++ types.hs
@@ -9,3 +9,7 @@ add3 x y z = x + y + z
 --Polymorphic Functions
 getLength :: [t] -> Int
 getLength list = length(list)
+
+--Typeclasses in Polymorphic Functions
+showSum :: (Num t, Show t) => [t] -> [Char]
+showSum list = show(sum(list))
