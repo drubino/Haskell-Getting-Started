07437edd61d2d8c78dacb6e3e69fd2399bf9a593
Douglas Rubino <rubinod@Corp.Domain>
2016-03-21 16:28:36 -0400

    Implementing Eq

--- TypeClasses.hs
+++ TypeClasses.hs
@@ -1,3 +1,18 @@
 module TypeClasses(
-    
+    contains,    
+    RGB(..)
 ) where
+
+--Requires the Eq type class context for the type parameter
+contains :: (Eq t) => t -> [t] -> Bool
+contains _ [] = False 
+contains element (head : tail) 
+    | element == head   = True
+    | otherwise         = contains(element)(tail)
+    
+--Implementing the Eq type class
+data RGB = RGB Int Int Int
+instance Eq RGB where
+    (RGB r1 g1 b1) == (RGB r2 g2 b2) = 
+        (r1 == r2) && (g1 == g2) && (b1 == b2)
+        
