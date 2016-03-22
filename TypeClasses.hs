310771642f31fed4453ac1cf4827b52568d50e53
Douglas Rubino <rubinod@Corp.Domain>
2016-03-22 10:57:31 -0400

    Implementing Show

--- TypeClasses.hs
+++ TypeClasses.hs
@@ -1,6 +1,7 @@
 module TypeClasses(
     contains,    
-    RGB(..)
+    RGB(..),
+    contains
 ) where
 
 --Requires the Eq type class context for the type parameter
@@ -10,9 +11,14 @@ contains element (head : tail)
     | element == head   = True
     | otherwise         = contains(element)(tail)
     
---Implementing the Eq type class
+--Implementing the Eq and Show type classes
 data RGB = RGB Int Int Int
 instance Eq RGB where
     (RGB r1 g1 b1) == (RGB r2 g2 b2) = 
         (r1 == r2) && (g1 == g2) && (b1 == b2)
-        
+instance Show RGB where
+    show (RGB r g b) = 
+        "RGB" ++ " " ++ 
+        (show(r)) ++ " " ++ 
+        (show(g)) ++ " " ++ 
+        (show(b))
