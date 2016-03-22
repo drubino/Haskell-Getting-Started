631ba214816ef6a5ac0acae30be534b423d55d42
Douglas Rubino <rubinod@Corp.Domain>
2016-03-22 15:24:05 -0400

    Set example

--- Collections.hs
+++ Collections.hs
@@ -1,2 +1,9 @@
-module Collections where 
+module Collections(
+    module Data.Set
+) where 
+import Data.Set
 
+--Set
+toSet list = toSet'(list)(empty)
+toSet' (first : rest) set = toSet'(rest)(insert(first)(set))
+toSet' [] set = set
