21232bf2f4afa69ae9e4ac44e17e8c120128a0d2
Douglas Rubino <rubinod@Corp.Domain>
2016-03-22 15:29:39 -0400

    Updated imports

--- Collections.hs
+++ Collections.hs
@@ -1,9 +1,16 @@
 module Collections(
-    module Data.Set
+    module S,
+    module M,
+    toSet
 ) where 
-import Data.Set
+    
+import qualified Data.Set as S 
+import qualified Data.Map as M
 
 --Set
-toSet list = toSet'(list)(empty)
-toSet' (first : rest) set = toSet'(rest)(insert(first)(set))
+toSet list = toSet'(list)(S.empty)
+toSet' (first : rest) set = toSet'(rest)(S.insert(first)(set))
 toSet' [] set = set
+
+--map
+
