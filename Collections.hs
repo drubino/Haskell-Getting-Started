96efede377b44d05d3bfe657e646eb567841796f
Douglas Rubino <rubinod@Corp.Domain>
2016-03-22 15:38:22 -0400

    Implemented toMap

--- Collections.hs
+++ Collections.hs
@@ -1,8 +1,4 @@
-module Collections(
-    module S,
-    module M,
-    toSet
-) where 
+module Collections where 
     
 import qualified Data.Set as S 
 import qualified Data.Map as M
@@ -13,4 +9,7 @@ toSet' (first : rest) set = toSet'(rest)(S.insert(first)(set))
 toSet' [] set = set
 
 --map
+toMap list = toMap'(list)(M.empty)
+toMap' ((k, v) : rest) map = toMap'(rest)(M.insert(k)(v)(map))
+toMap' [] map = map
 
