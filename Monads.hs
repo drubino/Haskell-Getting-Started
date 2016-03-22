5217e456b8d35cd65d7a6d58c28a8e3fa212f30a
Douglas Rubino <rubinod@Corp.Domain>
2016-03-22 16:53:35 -0400

    Implementing monad

--- Monads.hs
+++ Monads.hs
@@ -1,2 +1,12 @@
 module Monads where
 
+--Defining the Monad type class
+class Monad' m where
+    return' :: t -> m t
+    bind' :: m t -> (t -> m u) -> m u
+    
+instance Monad' [] where
+    return' t = [t]
+    bind' [] _ = []
+    bind' (first : rest) projection = projection(first) ++ bind'(rest)(projection)
+    
