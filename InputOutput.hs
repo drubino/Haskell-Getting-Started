46834edc5e62d1217674557fdd5af08c08b322f9
Douglas Rubino <rubinod@Corp.Domain>
2016-03-22 11:34:47 -0400

    Defining main

--- InputOutput.hs
+++ InputOutput.hs
@@ -1,3 +1,8 @@
 module InputOutput(
 ) where
-    
+
+--The program expects a main function
+main :: IO ()
+main = putStrLn("Hello World") -- Printed
+main2 :: IO ()
+main2 = putStrLn("Hello World") -- Not Printed 
