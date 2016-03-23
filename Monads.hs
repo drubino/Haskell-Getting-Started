e8efcd9f3ba5e778ef4fe3cb7eee998e402f7345
Douglas Rubino <rubinod@Corp.Domain>
2016-03-23 10:52:06 -0400

    addM, a function on monads

--- Monads.hs
+++ Monads.hs
@@ -4,9 +4,20 @@ module Monads where
 class Monad' m where
     return' :: t -> m t
     bind' :: m t -> (t -> m u) -> m u
-    
+
 instance Monad' [] where
     return' t = [t]
     bind' [] _ = []
     bind' (first : rest) projection = projection(first) ++ bind'(rest)(projection)
+    
+--Defining the add function for Monads
+addM :: (Monad m, Num n) => m n -> m n -> m n
+addM' :: (Monad m, Num n) => m n -> m n -> m n
+addM mx my = mx >>= (\x -> my >>= (\y -> return(x + y)))
+addM' mx my = do
+    x <- mx
+    y <- my
+    return(x + y)
+    
+
     
